e484f0f7c7b63b722db35db921071630e3593703
Greugreu <fenris.fitz@hotmail.fr>
2019-07-09 15:50:19 +0200

    Contact Slide
    ADD Symfony Encore bundle
    ADD npm server
    ADD yarn
    ADD app.css
    ADD app.js
    
    show.html.twig:
    -add default none to button and form
    
    base.html.twig:
    -add JS implementation via symfony bridge

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
